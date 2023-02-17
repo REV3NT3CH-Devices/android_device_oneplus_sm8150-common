13b2808740363948252d942c3b808d1e8c7658c2
srgrusso <srgrusso240@gmail.com>
2023-02-16 20:16:04 -0600

    sm8150-common: Remove some packages

---
+++ remove_packages/Android.mk
@@ -0,0 +1,20 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := RemovePackages
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_OVERRIDES_PACKAGES := \
+	Chrome \
+	Drive \
+	Maps \
+	Photos \
+	PixelLiveWallpaperPrebuilt \
+	PixelWallpapers2021 \
+	PrebuiltGmail \
+	RecorderPrebuilt \
+	WallpaperPickerGoogleRelease
+LOCAL_UNINSTALLABLE_MODULE := true
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_SRC_FILES := /dev/null
+include $(BUILD_PREBUILT)

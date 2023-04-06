2f96a0e71d413b2e7fe7a92f93f13a17adaa022a
REV3NT3CH <rev3nt3ch@gmail.com>
2023-04-06 07:58:14 -0700

    Lets remove more apps to free space

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -5,14 +5,24 @@ LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
 LOCAL_OVERRIDES_PACKAGES := \
+	CalculatorGooglePrebuilt \
+	CalendarGooglePrebuilt \
+	Camera2 \
 	Chrome \
+	Chrome-Stub \
+	DocumentsUIGoogle \
 	Drive \
+	FilesPrebuilt \
+	GoogleContacts \
 	Maps \
 	Photos \
 	PixelLiveWallpaperPrebuilt \
 	PixelWallpapers2021 \
+	PrebuiltDeskClockGoogle \
 	PrebuiltGmail \
 	RecorderPrebuilt \
+	ScribePrebuilt \
+	talkback \
 	WallpaperPickerGoogleRelease
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
